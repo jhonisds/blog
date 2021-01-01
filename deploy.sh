d53bbfadab1e107c28202202c6d1e23cdb319bd9
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-01-01 13:01:29 -0300

    deploy gigalixir process.

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+echo "Start deploy gigalixir...."
+git remote add gigalixir https://jhonisds17%40gmail.com:b13f8598-9e14-4117-8fd6-9ffa1c7a0dd3@git.gigalixir.com/jss-blog.git
+git push gigalixir main
+echo "Finish deploy...."
