ac73ce8ac646e664d821e9a5d4c30a4f1027975b
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-02-07 11:29:05 -0300

    Listing comments by socket

--- lib/blog_web/channels/comments_channel.ex
+++ lib/blog_web/channels/comments_channel.ex
@@ -1,20 +1,26 @@
 defmodule BlogWeb.CommentsChannel do
   use BlogWeb, :channel
 
-  alias Blog.Posts
+  alias Blog.{Comments, Posts}
 
-  def join("comments:" <> post_id, payload, socket) do
-    # IO.inspect(post_id, label: "ID")
-    # IO.inspect(payload, label: "PAYLOAD")
-    # IO.inspect(socket, label: "SOCKET")
-
+  def join("comments:" <> post_id, _payload, socket) do
     post = Posts.get_post_with_comments!(post_id)
-
-    IO.inspect(post.comments)
-
-    {:ok, %{comments: post.comments}, socket}
+    {:ok, %{comments: post.comments}, assign(socket, :post_id, post.id)}
   end
 
-  def handle_in() do
+  def handle_in("comment:add", content, socket) do
+    response =
+      socket.assigns.post_id
+      |> Comments.create_comment(content)
+
+    IO.inspect(response)
+
+    case response do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}}
+    end
   end
 end
