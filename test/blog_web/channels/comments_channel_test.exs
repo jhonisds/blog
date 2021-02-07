e7265060abe978f48b2b9fc4d6115d314c84c813
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-02-07 19:30:12 -0300

    Add new comment test

--- test/blog_web/channels/comments_channel_test.exs
+++ test/blog_web/channels/comments_channel_test.exs
@@ -6,15 +6,29 @@ defmodule BlogWeb.CommentsChannelTest do
 
   @valid_attrs %{title: "Phoenix", description: "Description Phoenix"}
 
-  test "should be connect to socket" do
+  setup do
     {:ok, post} = Posts.create_post(@valid_attrs)
     {:ok, socket} = connect(UserSocket, %{})
 
+    {:ok, socket: socket, post: post}
+  end
+
+  test "should be connect to socket", %{socket: socket, post: post} do
     {:ok, result, socket} = subscribe_and_join(socket, "comments:#{post.id}", %{})
 
     assert post.id == socket.assigns.post_id
     assert [] == result.comments
+  end
 
-    # IO.inspect(result)
+  test "create a new comment", %{socket: socket, post: post} do
+    {:ok, _result, socket} = subscribe_and_join(socket, "comments:#{post.id}", %{})
+
+    ref = push(socket, "comment:add", %{"content" => "abcd"})
+
+    msg = %{comment: %{content: "abcd"}}
+    assert_reply ref, :ok, %{}
+    broadcast_event = "comments:#{post.id}:new"
+    assert_broadcast broadcast_event, msg
+    refute is_nil(msg)
   end
 end
